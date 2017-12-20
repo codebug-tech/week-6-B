42af3dbd2b78fc270063c18d7430da597c9112ec
Brandon Sorbara <sorbarbj@Brandons-MBP-2.home>
2017-12-19 22:00:58 -0500

    week-6-b work

--- script.js
+++ script.js
@@ -4,18 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
   var options = ["rock", "paper", "scissors"];
   var computerChoice = options[Math.floor(Math.random() * options.length)];
   // Your prompt() goes below
+  var userChoice = prompt("rock, paper, scissors?");
 
-
-
-
-
-
+  console.log("The user chooses: " + userChoice);
   console.log("The computer chooses: " + computerChoice);
   // Your declareWinner function goes below
+  function declareWinner(userChoice, computerChoice) {
+    if ((userChoice == "paper" && computerChoice == "scissors") || (userChoice == "scissors" && computerChoice == "rock") || (userChoice == "rock" && computerChoice == "paper")) {
+      return "The computer won!";
+    }
+    else if ((userChoice == "scissors" && computerChoice == "paper") || (userChoice == "rock" && computerChoice == "scissors") || (userChoice == "paper" && computerChoice == "rock")) {
+      return "You won!";
+    }
+    else {
+      return "It's a tie!";
+    };
+  };
 
-
-
-
+  console.log(declareWinner(userChoice,computerChoice));
 
 
 
